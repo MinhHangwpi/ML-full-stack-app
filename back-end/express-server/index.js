b2527ba746210f3d76f94cf5318a0dbf8f054283
MinhHang <mgradetsky@wpi>
2023-10-09 14:21:33 -0400

    added docker for the express app

--- back-end/express-server/index.js
+++ back-end/express-server/index.js
@@ -2,16 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
 
-
 const app = express();
 
 // Middleware to parse JSON bodies
-app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.json({ limit: '5mb' }));
 
 // POST endpoint to receive data
 app.post('/landmarks', async (req, res) => {
   // Log the request body
-  console.log('Received data:', req.body);
+  // console.log('Received data:', req.body);
 
   try {
     // Forward the data to the Flask server
@@ -22,12 +21,8 @@ app.post('/landmarks', async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error('Error forwarding data:', error.message);
-    res.status(500).json({ message: 'Error forwarding data to Flask server' });
+    res.status(500).json({ message: 'Error forwarding data to Flask server' }); // need error handling for when the flask server is down.
   }
-
-  // console.log(req);
-  // // Respond to the client
-  // res.json({ message: 'Data received!' });  
 });
 
 // Start the server
@@ -38,9 +33,6 @@ app.listen(port, () => {
 
 
 /**
- * 
- * A sample front end request would be
- * 
+ * Sample client request:
+ * curl -X POST -H "Content-Type: application/json" -d @data.json http://localhost:5000/landmarks
  */
-// curl -X POST -H "Content-Type: application/json" -d '[{"x":0.35211437940597534,"y":1.0783183574676514,"z":9.194144467983278e-7},{"x":0.4362814426422119,"y":1.075021505355835,"z":-0.01458402443677187}]' http://localhost:3000/landmarks
-
