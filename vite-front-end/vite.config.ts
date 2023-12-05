c8cd7dbc9215187add28b8114e86ebecf719701c
MinhHang <mgradetsky@wpi>
2023-12-05 01:19:00 -0500

    added CI/CD code

--- vite-front-end/vite.config.ts
+++ vite-front-end/vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    // port: 443, // you can replace this port with any port
     port:5173
   }
 }
